79cfeb54eb9f1e77975f75167faa6a354f57f4e8
tee-lusk <tee-lusk@users.noreply.github.com>
2021-10-12 17:16:41 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('616610e9821a47e722b81343')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    `mongodb+srv://admin:${process.env.MONGO_PASS}@cse341.w7n3c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Tyler',
+          email: 'tyler@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
